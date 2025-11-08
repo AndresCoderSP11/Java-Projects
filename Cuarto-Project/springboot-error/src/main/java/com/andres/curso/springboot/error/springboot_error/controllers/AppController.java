c560411810f8318f3ab58d13d9ae8fd030cf5bd8
AndresSP11 <andres.salazar.p@uni.pe>
2025-11-08 18:47:17 -0500

    agrega

---
+++ Cuarto-Project/springboot-error/src/main/java/com/andres/curso/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,18 @@
+package com.andres.curso.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class AppController {
+    
+    @GetMapping("/app")
+    
+    public String index(){
+        int value = 100/0;
+        return "ok 200";
+    }
+
+}
