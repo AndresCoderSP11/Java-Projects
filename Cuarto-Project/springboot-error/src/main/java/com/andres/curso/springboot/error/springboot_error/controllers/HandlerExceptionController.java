6449b23a51b14dd83f0d32f36614f49cfb69521b
AndresSP11 <andres.salazar.p@uni.pe>
2025-11-11 00:00:23 -0500

    Agrega

---
+++ Cuarto-Project/springboot-error/src/main/java/com/andres/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -0,0 +1,62 @@
+package com.andres.curso.springboot.error.springboot_error.controllers;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import com.andres.curso.springboot.error.springboot_error.models.Error;
+import java.util.Map;
+
+@RestControllerAdvice
+public class HandlerExceptionController {
+    
+    @ExceptionHandler({ArithmeticException.class})
+    public ResponseEntity<Error> divisionByZero(Exception ex){
+
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error de aritmetica");
+        error.setMessage(null);
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String,Object> numberFormatException(Exception ex){
+        
+        Map<String,Object> error = new HashMap<>();
+        error.put("date", new Date().toString());
+        error.put("error","numero invalido o incorrecto, no tiene formato de digito!");
+        error.put("message",ex.getMessage());
+        error.put("status",HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
+
+    }
+
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+
+        Error error =new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado");
+        error.setMessage(e.getMessage());
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(404).body(error);
+
+    }
+
+}
---
+++ Cuarto-Project/springboot-error/src/main/java/com/andres/curso/springboot/error/springboot_error/services/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.andres.curso.springboot.error.springboot_error.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.andres.curso.springboot.error.springboot_error.models.domain.User;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private List<User> users;
+
+    public UserServiceImpl(){
+        this.users=new ArrayList<>();
+        users.add(new User(1L,"Pepe","Gonzales"));
+        users.add(new User(2L,"Andres","Mensa"));
+        users.add(new User(3L,"Maria","Perez"));
+        users.add(new User(4L,"Josefa","Reamirez"));
+        users.add(new User(5L,"Ale","Gutierrez"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        User user = null;
+        for(User u:users){
+            if(u.getId().equals(id)){
+                user=u;
+                break;
+            }
+        }
+
+        return null;
+    }
+    
+}
